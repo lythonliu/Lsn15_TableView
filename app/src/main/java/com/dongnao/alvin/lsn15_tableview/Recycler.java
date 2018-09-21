1bc1cf5ff92b58aa04fc6b151104f5725fe99821
Lython <liu.junli@moxiangroup.com>
2018-09-21 11:12:03 +0800

    init

---
+++ app/src/main/java/com/dongnao/alvin/lsn15_tableview/Recycler.java
@@ -0,0 +1,32 @@
+package com.dongnao.alvin.lsn15_tableview;
+
+import android.view.View;
+
+import java.util.Stack;
+
+/**
+ * Created by Administrator on 2017/6/5 0005.
+ */
+
+public class Recycler {
+    private Stack<View> [] views;
+    //打造一个回收池
+    public Recycler(int type) {
+        views=new Stack[type];
+        for (int i=0;i<type;i++) {
+            views[i]=new Stack<View>();
+        }
+    }
+
+    public void addRecycledView(View view,int type){
+            views[type].push(view);
+    }
+
+    public View getRecyclerdView(int type) {
+        try {
+            return views[type].pop();
+        } catch (Exception e) {
+            return null;
+        }
+    }
+}
